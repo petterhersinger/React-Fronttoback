a6ea418cdc37e4071b0417e409a5c8b6fe91d1d9
Petter Hersinger <petterhersinger@gmail.com>
2022-07-18 21:40:45 +0200

    spinner load

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,22 +2,23 @@ import React from "react"
 import {useContext} from "react"
 import FeedbackItem from "./FeedbackItem"
 import FeedbackContext from "../context/FeedbackContext"
+import Spinner from "./shared/Spinner"
 
 function FeedbackList() {
-    const {feedback} = useContext(FeedbackContext)
+    const {feedback, isLoading} = useContext(FeedbackContext)
 
-    if (!feedback || feedback.length === 0) {
-        return <p>no feedback yet</p>
+    if (!isLoading && (!feedback || feedback.length === 0)) {
+        return <p>No Feedback Yet</p>
 
     }
-    return (
-        <div className="feedback-list">
-            {feedback.map((item) => (
-                <FeedbackItem key={item.id} item={item}
-                />
-            ))}
-        </div>
-    )
-}
 
+    return isLoading ? <Spinner /> : (
+    <div className="feedback-list">
+    {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item}
+        />
+    ))}
+</div>
+)
+}
 export default FeedbackList
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,32 +1,30 @@
 import { v4 as uuidv4} from "uuid"
 import React from "react"
-import {createContext, useState} from "react"
+import {createContext, useState, useEffect} from "react"
 
 const FeedbackContext = createContext()
 
-export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: "Feedback item 1",
-            rating: 10,
-        },
-        {
-            id: 2,
-            text: "Feedback item 2",
-            rating: 9,
-        },
-        {
-            id: 3,
-            text: "Feedback item 3",
-            rating: 7,
-        },
-    ])
-
+export const FeedbackProvider = ({ children }) => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [feedback, setFeedback] = useState([])
     const [feedbackEdit, setFeedbackEdit] = useState ({
         item: {},
         edit: false
     })
+
+    useEffect(() => {
+        fetchFeedback()
+    }, [])
+
+    //Fetch feedback
+
+    const fetchFeedback = async () => {
+        const response = await fetch(`/feedback?_sort=id&_order=desc`)
+        const data = await response.json()
+
+        setFeedback(data)
+        setIsLoading(false)
+    }
 
 
     const addFeedback = (newFeedback) => {
@@ -60,7 +58,8 @@ export const FeedbackProvider = ({children}) => {
         addFeedback,
         editFeedback,
         feedbackEdit,
-        updateFeedback
+        updateFeedback,
+        isLoading
 
     }}> 
         {children}
